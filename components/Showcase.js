98d9eb8d9f7907866b2dfee4f4b5a893a1ff6f0f
Ahmed Touzani <ahmedbbccaa@gmail.com>
2021-05-09 04:03:02 +0000

    test github

--- components/Showcase.js
+++ components/Showcase.js
@@ -5,7 +5,7 @@ const Showcase = () => {
     return (
         <div className={styles.showcase}>
         <h1>Welcome To The Party!</h1>
-        <h2>Find the hottest DJ events</h2>
+        <h2>Find the hottest DJ events  </h2>
       </div>
     );
 }

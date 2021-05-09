a557335a26c2ed66009898d6e11281118a77ce39
Ahmed Touzani <ahmedbbccaa@gmail.com>
2021-05-09 03:47:41 +0000

    add images and some trucs

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
+import { useRouter} from  'next/router'
 import styles from '../styles/Layout.module.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Showcase from '../components/Showcase'
 
 const Layout = ({title,keywords,desciption,children}) => {
+    const router = useRouter()
  
     return (
         <div>
@@ -14,6 +17,8 @@ const Layout = ({title,keywords,desciption,children}) => {
             </Head>
 
             <Header />
+            {router.pathname === '/' && <Showcase/> }
+            
 
             <div className={styles.container}>
                 {children}
